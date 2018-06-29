9fc6c4e3ad70c8b8e565e6eadfce267946d4e488
Maciek Adamus <melonik0.9846@gmail.com>
2018-06-29 22:14:34 +0200

    Futher heroku tweaks and fixes

--- dist/server.js
+++ dist/server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use('/', express.static('dist'));
+app.use("/", express.static("servroot"));
 app.listen(port, () => console.info("Listening on port "+port));
